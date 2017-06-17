71fda8b4c03fbe07c02bbef2d5c882128c68666c
osama1225 <eng.osama12@gmail.com>
2017-06-17 23:34:01 +0200

    - call the appropriate facade methods in the normal controllers.

--- web/WEB-INF/src/com/crud/controllers/itemDetailsController.java
+++ web/WEB-INF/src/com/crud/controllers/itemDetailsController.java
@@ -1,9 +1,15 @@
 package com.crud.controllers;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.crud.dtos.ItemDTO;
+import com.crud.facades.ItemFacade;
 
 /**
  * 
@@ -14,9 +20,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/detail/i")
 public class itemDetailsController {
 
+	@Resource
+	private ItemFacade itemFacade;
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public String showItemDetails(final Model model) {
-		// TODO later
+	public String showItemDetails(final Model model, @PathVariable int id) {
+		ItemDTO item = itemFacade.getItemById(id);
+		model.addAttribute("item", item);
 		return "detail-view";
 	}
 
--- web/WEB-INF/src/com/crud/controllers/itemsListController.java
+++ web/WEB-INF/src/com/crud/controllers/itemsListController.java
@@ -1,9 +1,16 @@
 package com.crud.controllers;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.crud.dtos.ItemDTO;
+import com.crud.facades.ItemFacade;
 
 /**
  * 
@@ -11,14 +18,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
  *
  */
 @Controller
-@RequestMapping(value="/list")
-public class itemsListController{
+@RequestMapping(value = "/list")
+public class itemsListController {
+
+	@Resource
+	private ItemFacade itemFacade;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String showItems(final Model model) {
-		// TODO to be implemented later
+		List<ItemDTO> items = itemFacade.getAllItems();
+		model.addAttribute("items", items);
 		return "list-view";
 	}
 
-	
 }
